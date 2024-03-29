2c126a16fdff9b4289222e7fbd5c137776d237c8
devanify <yanevan03@gmail.com>
2024-03-29 15:37:41 +0800

    Belajar JS DOM - Event menggunakan addEvenetListener()

--- event/app.js
+++ event/app.js
@@ -10,3 +10,14 @@ const scream = () => {
 }
 button.onmouseover = scream
 
+const btnEvent = document.querySelector('#eventbtn')
+
+function stepOne(){
+	console.log('ini step satu')
+}
+function stepTwo(){
+	console.log('ini step dua')
+}
+//Jika kita mebuat cara biasa kita tidak bisa menjalankan 2 fungsi karena jika dideklarasikan, deklarasi paling terakhir akan mengganti nilai dari deklarasi diawal. Dengan menggunakan addEventListener kita bisa menjalankan 2 fungsi seperti berikut
+btnEvent.addEventListener('click', stepOne)
+btnEvent.addEventListener('click', stepTwo)
