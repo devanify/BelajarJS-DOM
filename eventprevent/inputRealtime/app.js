d265661e03d2810ed2da2205ec3a903a52e3829c
devanify <yanevan03@gmail.com>
2024-03-31 12:52:36 +0800

    Belajar JS DOM - Event Input Dan Change Pada Form

--- eventprevent/inputRealtime/app.js
+++ eventprevent/inputRealtime/app.js
@@ -2,6 +2,18 @@ const form = document.querySelector('#form')
 const input = document.querySelector('input')
 const list = document.querySelector('#notes')
 
+//Evnent CHANGE
+// input.addEventListener('change', (e) => {
+//     console.log('Nilai Berubah')
+// })
+
+//event input
+input.addEventListener('input', (e) => {
+    document.querySelector('h1').innerText =  input.value
+    console.log('Nilai Berubah')
+})
+
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     const noteValue = input.value
